e149b3b782124dfe91d154c133d139154df04469
Adam Miller <admiller@redhat.com>
2020-07-15 20:09:36 -0500

    set gnome settings in bootstrap, for some reason gsettings doesn't like it in Ansible
    Signed-off-by: Adam Miller <admiller@redhat.com>

---
+++ bootstrap-gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# unset GNOME Shell app switching
+gsettings set org.gnome.desktop.wm.keybindings switch-applications '[]'
+# change GNOME Shell window switching to alt+tab
+gsettings set org.gnome.desktop.wm.keybindings switch-windows '["<Alt>Tab"]'
+# change GNOME Shell window switching to alt+shift+tab
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward '["<Alt><Shift>Tab"]'
+# enable GNOME Fractional Scaling
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
