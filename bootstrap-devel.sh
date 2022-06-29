dc4cea3e876a77f55f0df871d2620a3403381791
Adam Miller <admiller@redhat.com>
2022-06-29 09:31:20 -0500

    move to new netcommon repo name
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-devel.sh
+++ bootstrap-devel.sh
@@ -111,7 +111,7 @@ git_repos=(
     "${collections_dir}/trendmicro/deepsecurity git@github.com:maxamillion/ansible_collections.trendmicro.deepsecurity.git https://github.com/ansible-security/ansible_collections.deepsecurity.deepsecurity.git"
     "${collections_dir}/crowdstrike/falcon git@github.com:maxamillion/ansible_collections.crowdstrike.falcon.git https://github.com/ansible-security/ansible_collections.crowdstrike.falcon.git"
     "${collections_dir}/ansible/posix git@github.com:maxamillion/ansible.posix.git https://github.com/ansible-collections/ansible.posix.git"
-    "${collections_dir}/ansible/netcommon git@github.com:maxamillion/netcommon.git https://github.com/ansible-collections/netcommon.git"
+    "${collections_dir}/ansible/netcommon git@github.com:maxamillion/ansible.netcommon.git https://github.com/ansible-collections/ansible.netcommon.git"
     "${collections_dir}/community/general git@github.com:maxamillion/community.general.git https://github.com/ansible-collections/community.general.git"
     "${collections_dir}/community/kubernetes git@github.com:maxamillion/community.kubernetes.git https://github.com/ansible-collections/community.kubernetes.git"
     "${collections_dir}/community/okd git@github.com:maxamillion/community.okd.git https://github.com/ansible-collections/community.okd.git"
