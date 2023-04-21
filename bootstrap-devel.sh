78a320d08bed72a58d06e2ddd582dbbfceda4961
Adam Miller <admiller@redhat.com>
2023-04-21 15:46:09 -0500

    add edge.test_utils
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-devel.sh
+++ bootstrap-devel.sh
@@ -132,6 +132,7 @@ git_repos=(
     "${collections_dir}/osbuild/composer git@github.com:maxamillion/osbuild.composer.git https://github.com/ansible-collections/osbuild.composer.git"
     "${collections_dir}/edge/workload git@github.com:maxamillion/edge.workload.git https://github.com/ansible-collections/edge.workload.git"
     "${collections_dir}/edge/microshift git@github.com:maxamillion/edge.microshift.git https://github.com/ansible-collections/edge.microshift.git"
+    "${collections_dir}/edge/test_utils git@github.com:maxamillion/edge.test_utils.git https://github.com/ansible-collections/edge.test_utils.git"
     "${collections_dir}/redhat_cop/controller_configuration git@github.com:maxamillion/controller_configuration.git https://github.com/redhat-cop/controller_configuration.git"
     "${collections_dir}/redhat_cop/ah_configuration git@github.com:maxamillion/ah_configuration.git https://github.com/redhat-cop/ah_configuration.git"
     "${collections_dir}/redhat_cop/ee_utilities git@github.com:maxamillion/ee_utilities.git https://github.com/redhat-cop/ee_utilities.git"
