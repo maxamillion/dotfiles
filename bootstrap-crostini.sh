4ad84ecc9e676c751554994bfd691767a3dcff7f
Adam Miller <admiller@redhat.com>
2023-08-09 16:45:33 -0500

    add docker, sadly podman doesn't work in crostini/podman
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -7,6 +7,18 @@ curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.l
 sudo apt update
 sudo apt install -y tailscale
 
+# docker
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # nodejs LTS
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt install -y nodejs
