f26b43603f3297f960fc1714449572910f7d589a
Adam Miller <admiller@redhat.com>
2023-08-16 16:03:40 -0500

    upgrade crostini from bullseye to bookworm
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -2,8 +2,8 @@
 source ./bootstrap-lib.sh
 
 # tailscale
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo apt update
 sudo apt install -y tailscale
 
