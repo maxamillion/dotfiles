90af8dc68a8aea541ba8fff5d055f8bb2cfb6e3e
Adam Miller <admiller@redhat.com>
2023-11-15 14:41:22 -0600

    refresh repos before installing nodejs
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-crostini.sh
+++ bootstrap-crostini.sh
@@ -43,6 +43,7 @@ if [[ $? -ne 0 ]]; then
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt update
     sudo apt install -y nodejs
 fi
 
