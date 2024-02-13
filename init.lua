d0a171e0404696e8a04092e428b9ddf048a13f8d
Adam Miller <admiller@redhat.com>
2024-02-12 22:08:22 -0600

    clean up init.lua, remove things I don't use, add ansible-vim
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- init.lua
+++ init.lua
@@ -117,10 +117,8 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
-
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  --
+  'pearofducks/ansible-vim',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
@@ -220,42 +218,20 @@ require('lazy').setup({
         end, { expr = true, desc = 'Jump to previous hunk' })
 
         -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
         -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
+        map('n', '<leader>gb', function()
           gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
+        end, { desc = '[g]it [b]lame line' })
+        map('n', '<leader>gd', gs.diffthis, { desc = '[g]it [d]iff against index' })
+        map('n', '<leader>gD', function()
           gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
+        end, { desc = '[g]it [D]iff against last commit' })
 
         -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+        map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = '[g]it [t]oggle git [b]lame line' })
+        map('n', '<leader>gtd', gs.toggle_deleted, { desc = '[g]it [t]oggle git show [d]deleted' })
       end,
     },
-  },
-  {
-    -- colorscheme
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
   },
   {
     -- colorscheme
@@ -421,13 +397,13 @@ vim.defer_fn(function()
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
 
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+    -- Autoinstall languages that are not installed. 
     auto_install = false,
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
     -- List of parsers to ignore installing
     ignore_install = {},
-    -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+    -- Additional Treesitter modules here:
     modules = {},
     highlight = { enable = true },
     indent = { enable = true },
@@ -488,7 +464,7 @@ vim.defer_fn(function()
 end, 0)
 
 
--- treesitter code contxt
+-- treesitter code context
 require('treesitter-context').setup {
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 }
