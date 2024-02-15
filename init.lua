e4b40b3dda0ec046e1f93798c90ea63e19a95655
Adam Miller <admiller@redhat.com>
2024-02-15 14:14:20 -0600

    use C-y to accept cmp suggestions
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- init.lua
+++ init.lua
@@ -642,7 +642,7 @@ cmp.setup {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-y>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
