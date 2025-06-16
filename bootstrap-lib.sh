5502d8ed1885c33d0f5c531dcf7d439636dd0da1
Adam Miller <admiller@redhat.com>
2025-06-16 11:19:11 -0500

    update add-repo syntax for dnf5
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-lib.sh
+++ bootstrap-lib.sh
@@ -183,14 +183,16 @@ fn_system_install_tailscale() {
         if ! rpm -q tailscale &>/dev/null; then
             local el_major_version
             el_major_version=$(rpm -E %rhel)
-            sudo dnf config-manager --add-repo "https://pkgs.tailscale.com/stable/rhel/${el_major_version}/tailscale.repo"
+            # sudo dnf config-manager --add-repo "https://pkgs.tailscale.com/stable/rhel/${el_major_version}/tailscale.repo"
+            sudo dnf config-manager addrepo "https://pkgs.tailscale.com/stable/rhel/${el_major_version}/tailscale.repo"
             sudo dnf install -y tailscale || fn_log_error "${FUNCNAME[0]}: failed to install tailscale"
             sudo systemctl enable --now tailscaled || fn_log_error "${FUNCNAME[0]}: failed to enable tailscale.service"
         fi
     fi
     if [[ "${ID}" == "fedora" ]]; then
         if ! rpm -q tailscale &>/dev/null; then
-            sudo dnf config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+            # sudo dnf config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+            sudo dnf config-manager addrepo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
             sudo dnf install -y tailscale || fn_log_error "${FUNCNAME[0]}: failed to install tailscale"
             sudo systemctl enable --now tailscaled || fn_log_error "${FUNCNAME[0]}: failed to enable tailscale.service"
         fi
