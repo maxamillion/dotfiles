7fa9206cb734a55f97fac0c5a9675005a083c091
Adam Miller <admiller@redhat.com>
2025-07-29 10:07:23 -0500

    add fractal, epiphany/web, and signal to flatpak installs
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- bootstrap-lib.sh
+++ bootstrap-lib.sh
@@ -637,6 +637,9 @@ fn_flathub_install() {
         "io.podman_desktop.PodmanDesktop"
         "com.google.Chrome"
         "com.github.tchx84.Flatseal"
+        "org.gnome.Epiphany"
+        "org.gnome.Fractal"
+        "org.signal.Signal"
     )
     if ! flatpak remotes --user | grep flathub &>/dev/null; then
         flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo \
